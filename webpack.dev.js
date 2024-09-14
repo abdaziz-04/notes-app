ed7c5d702ad87398acd38085b3c83084068d22e2
abdaziz04 <abdaziz1713@gmail.com>
2024-09-14 23:50:59 +0700

    install webpack

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+});
